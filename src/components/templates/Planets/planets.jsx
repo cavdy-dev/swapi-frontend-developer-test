4a8cf307bf8abee325535dd9036c6f55c10fd21d
Cavdy <cavdyofficials@gmail.com>
2020-02-08 20:31:34 +0100

    planets api

--- src/components/templates/Planets/planets.jsx
+++ src/components/templates/Planets/planets.jsx
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 import WindowSizeListener from 'react-window-size-listener';
 import Slider from 'react-slick';
 import PlanetCard from '../../molecules/PlanetCard/planetCard';
 import ContentTitle from '../../atoms/ContentTitle/contentTitle';
 import { popularPlanets, planets } from '../../../utils/strings';
-import { randomImg } from '../../../utils/helpers';
+import { randomImg, objectEmpty, splitURL } from '../../../utils/helpers';
+import getPlanets from '../../../services/Actions/PlanetsAction';
 
-const Planets = () => {
+const Planets = ({ getPlanets, planetsData }) => {
   const [screenWidth, setScreenWidth] = useState(window.screen.width);
   const [slides, setSlides] = useState(3);
+  const [data, setData] = useState({});
   const settings = {
     className: 'planets',
     dots: true,
@@ -17,6 +20,15 @@ const Planets = () => {
     slidesToShow: slides,
     slidesToScroll: slides
   };
+
+  useEffect(() => {
+    getPlanets();
+  }, []);
+  useEffect(() => {
+    if (!objectEmpty(planetsData)) {
+      setData(planetsData);
+    }
+  }, [planetsData]);
 
   useEffect(() => {
     if (screenWidth <= 560) {
@@ -37,18 +49,26 @@ const Planets = () => {
       />
       <ContentTitle title={popularPlanets} />
       <Slider {...settings}>
-        <PlanetCard id={1} image={planets[randomImg(planets)]} title="" />
-        <PlanetCard id={2} image={planets[randomImg(planets)]} title="" />
-        <PlanetCard id={3} image={planets[randomImg(planets)]} title="" />
-        <PlanetCard id={4} image={planets[randomImg(planets)]} title="" />
-        <PlanetCard id={5} image={planets[randomImg(planets)]} title="" />
-        <PlanetCard id={6} image={planets[randomImg(planets)]} title="" />
-        <PlanetCard id={7} image={planets[randomImg(planets)]} title="" />
-        <PlanetCard id={8} image={planets[randomImg(planets)]} title="" />
-        <PlanetCard id={9} image={planets[randomImg(planets)]} title="" />
+        {!objectEmpty(data) &&
+          data.results.map((result, i) => {
+            if (i < 9) {
+              return (
+                <PlanetCard
+                  key={result.name}
+                  url={splitURL(result.url)}
+                  image={planets[randomImg(planets)]}
+                  title={result.name}
+                />
+              );
+            }
+          })}
       </Slider>
     </>
   );
 };
 
-export default Planets;
+const mapStateToProps = state => ({
+  planetsData: state.planets
+});
+
+export default connect(mapStateToProps, { getPlanets })(Planets);
